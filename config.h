1a32fcea14efe6f5f53d41425e55fdbebc8cf460
alexsyss@hotmail.com <alexsyss@hotmail.com>
2021-01-18 12:08:21 -0300

    18 ene 2021

--- config.h
+++ config.h
@@ -2,13 +2,13 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	//{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	/* {"",	"crypto",	0,	13}, */
-	/* {"",	"torrent",	20,	7}, */
+	/* {"",	"sb-crypto",	0,	13}, */
+	/* {"",	"sb-torrent",	20,	7}, */
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	{"",	"music",	0,	11},
-	{"",	"news",		0,	6},
-	{"",	"mailbox",	180,	12},
-	{"",	"pacpackages",	0,	8},
+	{"",	"sb-music",	0,	11},
+	{"",	"sb-news",		0,	6},
+	{"",	"sb-mailbox",	180,	12},
+	{"",	"sb-pacpackages",	0,	8},
 	/* {"",	"sda6",	10,	14}, */
 	/* {"",	"price bat \"Basic Attention Token\" 🦁",	0,	20}, */
 	/* {"",	"price btc Bitcoin 💰",				0,	21}, */
@@ -20,15 +20,15 @@ static const Block blocks[] = {
 	/* {"",	"moonphase",	18000,	17}, */
 /*	 {"",	"nettraf",	1,	16}, */
 	/* {"",	"forecast",	18000,	5}, */
-	{"",	"forecast",	0,	5},
+	{"",	"sb-forecast",	0,	5},
 	 /* {"", 	"nfp", 		18000, 	21}, */
 	 /* {"", 	"nfp", 		0, 	21}, */
 	/*{"",	"battery | tr \'\n\' \' \'",	5,	3}, */
-	{"",	"audioout",	0,	13},
+	{"",	"sb-audioout",	0,	13},
 	/* {"",	"kbselect",	0,	30}, */
-	{"",	"volume",	0,	10},
-	{"",	"clock",	60,	1},
-	{"",	"internet",	5,	4},
+	{"",	"sb-volume",	0,	10},
+	{"",	"sb-clock",	60,	1},
+	{"",	"sb-internet",	5,	4},
 	/* {"",	"help-icon",	0,	15}, */
 };
 
