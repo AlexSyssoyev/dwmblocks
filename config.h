4138f3bb04b5520846ba026db8da9b084b627b99
alexsyss@hotmail.com <alexsyss@hotmail.com>
2021-02-25 02:54:22 -0300

    25 feb 2021

--- config.h
+++ config.h
@@ -30,8 +30,8 @@ static const Block blocks[] = {
 	/*{"",	"sb-internet",	5,	4}, */
 	/*/1* {"",	"help-icon",	0,	15}, *1/ */
 	/* {"⌨", "sb-kbselect", 0, 30}, */
-	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	{"",	"sb-tasks",	10,	26},
+	/* {"", "cat /tmp/recordingicon 2>/dev/null",	0,	9}, */
+	/* {"",	"sb-tasks",	10,	26}, */
 	{"",	"sb-music",	0,	11},
 	{"",	"sb-pacpackages",	0,	8},
 	{"",	"sb-news",		0,	6},
@@ -41,7 +41,7 @@ static const Block blocks[] = {
 	/* {"",	"sb-price xmr \"Monero\" 🔒",			9000,	24}, */
 	/* {"",	"sb-price eth Ethereum 🍸",	9000,	23}, */
 	/* {"",	"sb-price btc Bitcoin 💰",				9000,	21}, */
-	{"",	"sb-torrent",	20,	7},
+	/* {"",	"sb-torrent",	20,	7}, */
 	/* {"",	"sb-memory",	10,	14}, */
 	/* {"",	"sb-cpu",		10,	18}, */
 	/* {"",	"sb-moonphase",	18000,	17}, */
@@ -52,7 +52,7 @@ static const Block blocks[] = {
 	{"",	"sb-volume",	0,	10},
 	{"",	"sb-clock",	60,	1},
 	{"",	"sb-internet",	5,	4},
-	{"",	"sb-help-icon",	0,	15},
+	/* {"",	"sb-help-icon",	0,	15}, */
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
